191b92e57d8341b64f3ceb522a6d9ca80d8b821a
Ztoupis Konstantinos <you@example.com>
2021-10-27 17:23:06 +0300

    remove advanced

--- redux/workshop/src/examples/05/store/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = (store) => (next) => (action) => {
-  console.log("dispatching", action);
-  next(action);
-  console.log("curent state", store.getState());
-};
-
-export default logger;
--- redux/workshop/src/examples/solution1/constants/todosTypes.js
+++
@@ -1,3 +0,0 @@
-export const ADD_TODO = "[todos] - add todo";
-export const TOGGLE_TODO = "[todos] - toggle todo";
-export const DELETE_TODO = "[todos] - delete todo";
